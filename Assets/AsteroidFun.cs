ec6eed45b571edac15712aa9105e89be8a2493cc
IanRickles <iangrickles@gmail.com>
2019-04-08 21:02:45 -0400

    Add files via upload

---
+++ Assets/AsteroidFun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidFun : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(Random.Range(-8, 8), Random.Range(-5, 5));
+        transform.Rotate(0, 0, Random.value * 360);
+        GetComponent<Rigidbody2D>().AddForce(transform.up * (Random.value*50f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = myMath.Wrap(transform.position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+
+            RocketPhysics.numberOfAsteroids--;
+            RocketPhysics.score += 5;
+            
+        }
+        GameObject[] asteroidOnScreen = GameObject.FindGameObjectsWithTag("Asteroid");
+        if (collision.gameObject.tag == "PlayerRocket")
+        {
+            foreach (GameObject asteroid in asteroidOnScreen)
+            {
+                if (Vector2.Distance(asteroid.transform.position,transform.position) <= 3f)
+                {
+                    Destroy(collision.gameObject);
+                    Destroy(asteroid);
+                    RocketPhysics.numberOfAsteroids--;
+                    RocketPhysics.score += 5;
+                }
+            }
+        }
+    }
+}
